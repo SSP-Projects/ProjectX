8580733490a5cb0cc3e1dd027529534d7e397b64
ThePelican18 <danielnj18@gmail.com>
2021-01-03 00:13:28 +0100

    Modelo de prueba de la tabla centros añadido

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import uuid
+class Centers(models.Model):
+    # Campos
+    center_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=50, help_text="Ingrese el nombre del género")
+    address = models.CharField(max_length=100)
+    phone_number = models.BigIntegerField(max_length=9)
+    CIF = models.CharField(max_length=9)
+    email = models.EmailField(max_length=50)
+    image = models.ImageField(blank =True)
 
-# Create your models here.
+'''    # Metadata
+    class Meta:
+        ordering = ["center_id","CIF","name","address","phone_number","email"]
+    
+    # Métodos
+    def get_absolute_url(self):
+         """
+         Devuelve la url para acceder a una instancia particular de MyModelName.
+         """
+         return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        """
+        return self.field_name'''
