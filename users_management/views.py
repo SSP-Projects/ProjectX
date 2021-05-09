6ff3a1b913ed12faa2a0daade91243df15af748b
ThePelican18 <danielnj18@gmail.com>
2021-05-09 19:51:20 +0200

    home page doine

--- users_management/views.py
+++ users_management/views.py
@@ -11,8 +11,10 @@ def register(request):
 @login_required(login_url='/accounts/login/')
 def home(request):
     username = ""
+    app_tittle = "SIGNET"
+    studycenter_name = "GMQ CENTER"
     form = forms.TicketForm()
-    return render(request, 'middle/home.html', context={"studycenter_name":"GMQ CENTER","username":username,'form': form})
+    return render(request, 'middle/home.html', context={"studycenter_name":studycenter_name,"app_tittle":app_tittle,"username":username,'form': form})
 
 @login_required(login_url='/accounts/login/')
 def admin(request):
