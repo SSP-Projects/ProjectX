72b30c1ca6be217c18184c147deb6b9ecec49871
Sergio Garcia Mayo <garciamayosergio@gmail.com>
2021-05-15 14:36:19 +0200

    cambios

--- users_management/views.py
+++ users_management/views.py
@@ -8,7 +8,7 @@ def register(request):
     form = forms.RegisterForm()
     return render(request, 'register.html', context={'form': form})
 
-@login_required(login_url='/accounts/login/')
+#@login_required(login_url='/accounts/login/')
 def home(request):
     username = ""
     app_tittle = "SIGNET"
@@ -16,7 +16,7 @@ def home(request):
     form = forms.TicketForm()
     return render(request, 'middle/home.html', context={"studycenter_name":studycenter_name,"app_tittle":app_tittle,"username":username,'form': form})
 
-@login_required(login_url='/accounts/login/')
+#@login_required(login_url='/accounts/login/')
 def admin(request):
     employees = Employee.objects.all()
     # employees[0].name = 'adawda'
