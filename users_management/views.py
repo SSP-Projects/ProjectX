e0f2fe79c2dfdbc1a080d7b0f4eb378029d27139
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2021-05-15 22:48:30 +0200

    addesd

--- users_management/views.py
+++ users_management/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from . import forms
-from .models import Employee
+from .models import Employee, Notification
 
 
 def register(request):
@@ -13,7 +13,7 @@ def home(request):
     username = ""
     app_tittle = "SIGNET"
     studycenter_name = "GMQ CENTER"
-    form = forms.TicketForm()
+    form = forms.NotificationForm()
     return render(request, 'middle/home.html', context={"studycenter_name":studycenter_name,"app_tittle":app_tittle,"username":username,'form': form})
 
 @login_required(login_url='/accounts/login/')
@@ -23,3 +23,14 @@ def admin(request):
     # employees[0].save()
     # employees = Employee.objects.get(name='oeoe')
     return render(request, 'admin.html', context={'employees':employees})
+
+def send_notification(request):
+    if request.method == 'POST':
+        form = forms.NotificationForm(request.POST) 
+        if form.is_valid():
+            current_user = request.user
+            notification = form.save(commit=False)
+            notification.sender = request.user
+            if not current_user.is_superuser:
+                notification.receiver = request.user #TODO: get the admin of the system
+            notification.save()
