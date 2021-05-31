f9c7a403ea79529b37358ae7fdfe46cdf21c6961
Sergio Garcia Mayo <garciamayosergio@gmail.com>
2021-05-31 13:15:00 +0200

    filtrar fecha

--- users_management/views.py
+++ users_management/views.py
@@ -42,6 +42,24 @@ def home(request):
             })
     return redirect("/")
 
+def get_employee_job_interactions_date_range(request):
+    if request.is_ajax:
+        dni = request.GET['dni']
+        start_date = request.GET['startDate']
+        end_date = request.GET['endDate']
+        user = Employee.objects.get(dni=dni)
+
+        date_time_obj = datetime.strptime(end_date, '%Y-%m-%d')
+        end_date_obj = date_time_obj + timedelta(days=1)
+
+
+        employee_interactions = Interaction.objects.filter(employee = user, date_time__range=(start_date, end_date_obj)).order_by('-date_time')
+
+        interactions_json = serializers.serialize('json',employee_interactions)
+        return HttpResponse(interactions_json, content_type="application/json")
+    return None
+ 
+
 def get_employee_job_interactions_dni(request):
     if request.is_ajax:
         dni = request.GET['dni']
@@ -55,7 +73,7 @@ def get_employee_job_interactions_dni(request):
         interactions_json = serializers.serialize('json',employee_interactions)
         return HttpResponse(interactions_json, content_type="application/json")
     return None
-
+ 
 def getEmployeeInteractions(request):
 
     actual_employee = Employee.objects.get(user=request.user)
